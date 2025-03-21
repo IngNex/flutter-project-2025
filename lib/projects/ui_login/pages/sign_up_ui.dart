b490d4e2ce6497b754416593817e0328829716d0
IngNex <ingnexmaicol14@gmail.com>
2025-03-21 10:54:31 -0500

    update ui SignUp

--- lib/projects/ui_login/pages/sign_up_ui.dart
+++ lib/projects/ui_login/pages/sign_up_ui.dart
@@ -1,10 +1,206 @@
 import 'package:flutter/material.dart';
 
-class SignUpUI extends StatelessWidget {
+class SignUpUI extends StatefulWidget {
   const SignUpUI({super.key});
 
   @override
+  State<SignUpUI> createState() => _SignUpUIState();
+}
+
+class _SignUpUIState extends State<SignUpUI> {
+  bool obscureText = true;
+  bool obscureTextConfirm = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 224, 24, 24),
+              const Color.fromARGB(255, 78, 3, 101),
+            ],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.25,
+                padding: EdgeInsets.only(
+                  top: 60,
+                  bottom: 10,
+                  left: 30,
+                  right: 30,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Create You\nAccont',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.75,
+                padding: EdgeInsets.only(
+                  top: 60,
+                  bottom: 20,
+                  left: 30,
+                  right: 30,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          "Full Name",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.red.shade700,
+                          ),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: "Jhon Smith",
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Phone or Gmail",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.red.shade700,
+                          ),
+                        ),
+                        TextField(
+                          decoration:
+                              InputDecoration(hintText: "jsmith@gmail.com"),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Password",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.red.shade700,
+                          ),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: "********",
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  obscureText = !obscureText;
+                                });
+                              },
+                              icon: Icon(
+                                obscureText
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Confirm Password",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.red.shade700,
+                          ),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: "********",
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  obscureTextConfirm = !obscureTextConfirm;
+                                });
+                              },
+                              icon: Icon(
+                                obscureTextConfirm
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                            ),
+                          ),
+                          obscureText: obscureTextConfirm,
+                        ),
+                        SizedBox(height: 35),
+                        Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [
+                              const Color.fromARGB(255, 224, 24, 24),
+                              const Color.fromARGB(255, 78, 3, 101),
+                            ]),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              shadowColor: Colors.transparent,
+                              padding: EdgeInsets.all(12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                            ),
+                            child: Text(
+                              "SIGN UP",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      //mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Don't have an account?",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        ),
+                        Text(
+                          "Sign In",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
