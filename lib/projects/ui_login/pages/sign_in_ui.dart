093d456d6b53bc61754c0e60d08b217db1bb50f3
IngNex <ingnexmaicol14@gmail.com>
2025-03-20 19:36:48 -0500

    update ok sign in

--- lib/projects/ui_login/pages/sign_in_ui.dart
+++ lib/projects/ui_login/pages/sign_in_ui.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class SignInUI extends StatelessWidget {
+class SignInUI extends StatefulWidget {
   const SignInUI({super.key});
 
+  @override
+  State<SignInUI> createState() => _SignInUIState();
+}
+
+class _SignInUIState extends State<SignInUI> {
+  bool obscureText = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,58 +19,147 @@ class SignInUI extends StatelessWidget {
             const Color.fromARGB(255, 78, 3, 101),
           ]),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Text("Hello Sign in!"),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                decoration: BoxDecoration(color: Colors.white),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.25,
                 padding: EdgeInsets.only(
-                  top: 40,
+                  top: 60,
                   bottom: 10,
                   left: 30,
                   right: 30,
                 ),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Text("Gmail"),
-                    TextField(),
-                    const SizedBox(height: 10),
-                    Text("Password"),
-                    TextField(),
-                    const SizedBox(height: 10),
-                    Text('Forgot password?'),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        //Navigator.of(context).pushNamed(AppRoutes.signin);
-                      },
-                      style: ButtonStyle(
-                        padding: WidgetStatePropertyAll(EdgeInsets.all(15)),
-                        backgroundColor:
-                            WidgetStatePropertyAll(Colors.transparent),
-                        shape: WidgetStatePropertyAll(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
-                            side: BorderSide(width: 1, color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      child: Text(
-                        'Sign In'.toUpperCase(),
-                        style: TextStyle(color: Colors.white),
-                      ),
+                    Text(
+                      "Hello",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Sign in!",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
+              Container(
+                height: MediaQuery.of(context).size.height * 0.75,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                padding: EdgeInsets.only(
+                  top: 60,
+                  bottom: 10,
+                  left: 30,
+                  right: 30,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text("Gmail"),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "ejemplo@gmail.com",
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text("Password"),
+                        TextField(
+                          obscureText: obscureText,
+                          decoration: InputDecoration(
+                            labelText: "Ingrese texto",
+                            suffixIcon: IconButton(
+                              icon: Icon(obscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                              onPressed: () {
+                                setState(() {
+                                  obscureText = !obscureText;
+                                });
+                              },
+                            ), // Icono de "X" para limpiar
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          'Forgot password?',
+                          textAlign: TextAlign.end,
+                        ),
+                        const SizedBox(height: 50),
+                        Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                const Color.fromARGB(255, 224, 24, 24),
+                                const Color.fromARGB(255, 78, 3, 101),
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              shadowColor: Colors.transparent,
+                              padding: EdgeInsets.all(12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                            ),
+                            child: Text(
+                              'Sign In'.toUpperCase(),
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Don\'t have an account?',
+                          textAlign: TextAlign.end,
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          'Sign up',
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
